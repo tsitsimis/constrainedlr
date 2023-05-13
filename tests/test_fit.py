8d015a3f618f135a20b928fa0de621c968f98447
Theodore Tsitsimis <th.tsitsimis@gmail.com>
2023-05-13 20:26:50 +0300

    Add tests

---
+++ tests/test_fit.py
@@ -0,0 +1,86 @@
+import sys
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+
+from clr.model import ConstrainedLinearRegression
+
+atol = 1e-5
+
+dataset = load_diabetes()
+X = pd.DataFrame(dataset["data"], columns=dataset["feature_names"])
+y = dataset["target"]
+
+
+def test_no_intercept():
+    clr = ConstrainedLinearRegression(fit_intercept=False)
+    clr.fit(X, y)
+    assert clr.intercept_ is None
+    assert clr.coef_.shape[0] == X.shape[1]
+
+
+def test_intercept():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+    clr.fit(X, y)
+    assert clr.intercept_ is not None
+    assert clr.coef_.shape[0] == X.shape[1]
+
+
+def test_unconstrained():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+    clr.fit(X, y)
+
+    lr = LinearRegression(fit_intercept=True)
+    lr.fit(X, y)
+
+    assert np.allclose(lr.intercept_, clr.intercept_, atol=atol)
+    assert np.allclose(lr.coef_, clr.coef_, atol=atol)
+
+
+def test_all_positive():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+    clr.fit(X, y, features_sign_constraints={col: 1 for col in X.columns})
+
+    lr = LinearRegression(fit_intercept=True, positive=True)
+    lr.fit(X, y)
+
+    assert np.allclose(lr.intercept_, clr.intercept_, atol=atol)
+    assert np.allclose(lr.coef_, clr.coef_, atol=atol)
+
+
+def test_feature_signs():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+
+    # Perform multiple tests since signs are produced randomly
+    np.random.seed(0)
+    for _ in range(10):
+        signs = np.random.choice([-1, 1], size=X.shape[1])
+        features_sign_constraints = dict(zip(X.columns, signs))
+        clr.fit(X, y, features_sign_constraints=features_sign_constraints)
+
+        assert np.alltrue(np.sign(clr.coef_) == signs)
+
+
+def test_intercept_sign():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+    clr.fit(X, y, intercept_sign_constraint=1)
+    assert clr.intercept_ > 0
+
+    clr.fit(X, y, intercept_sign_constraint=-1)
+    assert clr.intercept_ < 0
+
+
+def test_features_sum():
+    clr = ConstrainedLinearRegression(fit_intercept=True)
+    features_sum_constraint_equal = 15
+    clr.fit(X, y, features_sum_constraint_equal=features_sum_constraint_equal)
+    sum_of_weights = clr.coef_.sum() + clr.intercept_
+    assert np.allclose(sum_of_weights, features_sum_constraint_equal, atol=atol)
+
+    clr = ConstrainedLinearRegression(fit_intercept=False)
+    features_sum_constraint_equal = 15
+    clr.fit(X, y, features_sum_constraint_equal=features_sum_constraint_equal)
+    sum_of_weights = clr.coef_.sum()
+    assert np.allclose(sum_of_weights, features_sum_constraint_equal, atol=atol)
